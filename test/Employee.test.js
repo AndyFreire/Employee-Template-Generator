99f99c3928824e742aae530f26ac126698f83d70
Andrew Freire <andrew.m.freire@gmail.com>
2020-09-06 17:24:15 -0500

    Corrected test scripts to all pass

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -7,19 +7,19 @@ test("Can instantiate Employee instance", () => {
 
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
-  const e = new Employee(name);
+  const e = new Employee(name, "email@gmail.com", 1);
   expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
+  const e = new Employee("Foo", "foo@gmail.com", testValue);
   expect(e.id).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee("Foo", testValue, 1);
   expect(e.email).toBe(testValue);
 });
 
@@ -31,13 +31,13 @@ test("Can get name via getName()", () => {
 
 test("Can get id via getId()", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
+  const e = new Employee("Foo", "foo@gmail.com", testValue);
   expect(e.getId()).toBe(testValue);
 });
 
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee("Foo", testValue, 1);
   expect(e.getEmail()).toBe(testValue);
 });
 
