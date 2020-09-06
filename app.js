45f47625bf19f36b990cca5cfae40a644db3eecb
Andrew Freire <andrew.m.freire@gmail.com>
2020-09-06 17:11:52 -0500

    Completed HTML generation with employee object arr

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
 const employeeArray = [];
 
 let managers = 0;
@@ -164,37 +167,52 @@ function gatherInternInfo(){
 
         // Reset the index and move to the next step
         currentIndex = 0;
-        console.log("Done gathering team info!")
+        console.log("Done gathering team info!");
+
+        console.log(OUTPUT_DIR);
+
+        if (fs.existsSync(OUTPUT_DIR)) {
+            writeHTMLFile();
+        } else {
+        
+            fs.mkdir(OUTPUT_DIR, (err) => { 
+                if (err) { 
+                    return console.error(err); 
+                } 
+                console.log('Directory created successfully!'); 
+                writeHTMLFile();
+            }); 
+        }
     }
 
 }
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 gatherInfo();
-
-
-
-
-
-
-
-
-
-
 
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+function writeHTMLFile(){
+
+    const html = render(employeeArray);
+
+    fs.writeFile(outputPath, html, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+
+}
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
